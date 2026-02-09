fcbeca5708db018d164bc4e685f87c722e3bcdc9
borashourya609-blip <borashourya609@gmail.com>
2026-02-10 00:00:48 +0530

    day9 program

---
+++ day9.c/q1.c
@@ -0,0 +1,14 @@
+//Mirror the Code Name
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s", s);
+
+    for (int i = strlen(s) - 1; i >= 0; i--) {
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
