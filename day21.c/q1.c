89827ea2da3c355a2876557d93e66101946e3f89
borashourya609-blip <borashourya609@gmail.com>
2026-02-21 23:38:18 +0530

    day 21 program

---
+++ day21.c/q1.c
@@ -0,0 +1,44 @@
+//Create and Traverse Singly Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n <= 0)
+        return 0;
+
+    struct node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for(int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = (struct node*)malloc(sizeof(struct node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;       
+            temp = newNode;
+        } else {
+            temp->next = newNode; 
+            temp = newNode;       
+        }
+    }
+
+    // Traverse and print
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
