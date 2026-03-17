5cc6464754d5a2131fd1f7c604499bff7181db65
borashourya609-blip <borashourya609@gmail.com>
2026-03-17 14:38:43 +0530

    day 29 program

---
+++ day29.c/q1.c
@@ -0,0 +1,97 @@
+// Rotate Linked List Right by k Places
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at end
+struct Node* insert(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Rotate right by k
+struct Node* rotate(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    struct Node* temp = head;
+    int n = 1;
+
+    // Find length and last node
+    while (temp->next != NULL) {
+        temp = temp->next;
+        n++;
+    }
+
+    // Make circular
+    temp->next = head;
+
+    // Reduce k
+    k = k % n;
+
+    int steps = n - k;
+
+    struct Node* newTail = head;
+
+    // Move to (n-k)th node
+    for (int i = 1; i < steps; i++) {
+        newTail = newTail->next;
+    }
+
+    struct Node* newHead = newTail->next;
+
+    // Break circle
+    newTail->next = NULL;
+
+    return newHead;
+}
+
+// Display list
+void display(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, k, x;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        head = insert(head, x);
+    }
+
+    scanf("%d", &k);
+
+    head = rotate(head, k);
+
+    display(head);
+
+    return 0;
+}
